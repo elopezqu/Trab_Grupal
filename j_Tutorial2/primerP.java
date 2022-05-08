57be5c83d593e1c8176a035ee3accf53351f6357
diego pacori <dpacoria@unsa.edu.pe>
2022-05-08 13:35:01 -0500

    Se explica las partes de lo basico de un archivo java e imprime hola mundo

---
+++ j_Tutorial2/primerP.java
@@ -0,0 +1,14 @@
+// Se crea la clase con el mismo nombre del archivo, en este caso primerP
+// Cerramos con las {} para definir un bloque
+public class primerP{
+    /*En un archivo java siempre se va a ejecutar el metodo main, por eso
+      siempre debemos crearlo dentro de la clase, podemos hacerlo manualmente o
+      con un atajo que es "psvm" + tab y se creara automaticamente
+    */
+    public static void main(String[] args) {
+        //Nuestro primero hola mundo
+        //Para imprimir algo debemos usar el 'System.out.println("cosas por escribir");'
+        //Cada declaracion de codigo necesita llevar al final el " ; "
+        System.out.println("Hello World");
+    }
+}
