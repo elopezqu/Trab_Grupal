1963ca78d992b7d91259890843fbf53868940b17
diego pacori <dpacoria@unsa.edu.pe>
2022-05-08 13:32:04 -0500

    Los pasos para crear un archivo java, compilarlo y ejecutarlo

---
+++ j_Tutorial2/Inicio2.java
@@ -0,0 +1,11 @@
+public class Inicio2{
+    public static void main(String[] args) {
+        /* 
+           Primero verificamos la version de java con "java -version"
+           luego creamos un archivo java "vim/code name.java"
+           trabajamos en el archivo, guardamos y compilamos con "javac name.java"
+           y para ejecutar "java name" todo estos comandos en la consola
+        */
+        System.out.println("Hello World");
+    }
+}
