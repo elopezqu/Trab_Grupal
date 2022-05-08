ce0025f0592338caa92f3d6c9b1bd0a8a9bced03
diego pacori <dpacoria@unsa.edu.pe>
2022-05-08 13:26:51 -0500

    Archivo de prueba

--- j_Tutorial/Inicio.java
+++ j_Tutorial/Inicio.java
@@ -1,10 +1,5 @@
 public class Inicio{
     public static void main(String[] args) {
-        /* Primero verificamos la version de java con "java -version"
-           luego creamos un archivo java "vim/code name.java"
-           trabajamos en el archivo, guardamos y compilamos con "javac name.java"
-           y para ejecutar "java name" todo estos comandos en la consola
-        */
-        System.out.println("Hello World");
+        System.out.println("Hola Mundo");
     }
 }
