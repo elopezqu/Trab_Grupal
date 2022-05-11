eed9b60a249e9cf887fa77218c25c65c8b8812a4
Edwin Francisco Aguilar Tancayo <eaguilartan@unsa.edu.pe>
2022-05-10 21:57:18 -0500

    lab01

---
+++ JavaTutorial/JavaOutput.java
@@ -0,0 +1,11 @@
+//JAVA OUTPUT
+//Programa de ejemplo de una salida de texto con el metodo PRINT
+
+public class JavaOutput {
+	public static void main (String[] args) {
+		
+		String str = new String("Hello World");
+		System.out.print(str+"\n");
+		
+	} 
+}
---
+++ JavaTutorial/JavaVariables.java
@@ -0,0 +1,18 @@
+//Java Variables
+
+public class JavaVariables {
+	public static void main (String[] args) {
+		
+		/* Declaracion de Variable
+		   <tipo de variable> nombreDeVariable ; */
+		String str1;
+		str1 = "EJEMPLO DE STRING 2";
+		
+		// <tipo de variable> nombreDeVariable = VALOR ;
+		String str2 = new String("EJEMPLO DE STRING 2");
+
+		System.out.print("-String 1: "+str1+"\n");
+		System.out.print("-String 2: "+str1+"\n");
+
+	}
+}
